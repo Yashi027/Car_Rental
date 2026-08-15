374557480dbac81cc0b4ae28a2118c7c9523790d
Yashi Bansal <bansalyashi163@gmail.com>
2026-08-15 19:30:24 +0530

    hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import { useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -8,6 +13,13 @@ const App = () => {
   return (
     <div >
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+      </Routes>
     </div>
   );
 }
