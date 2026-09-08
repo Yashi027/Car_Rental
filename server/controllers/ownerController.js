d77024f5a0bcb65be067046cbad87e32ee033753
Yashi Bansal <bansalyashi163@gmail.com>
2026-09-08 21:22:05 +0530

    adding car controller and route

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,13 +1,71 @@
-import User from "../models/User";
+import User from "../models/User.js";
+import Car from "../models/Car.js";
+import imageKit from "../configs/imageKit.js";
 
-
-export const changeRoleToOwner = async (req,res) => {
+export const changeRoleToOwner = async (req, res) => {
     try {
-        const {_id} = req.user;
-        await User.findByIdAndUpdate(_id, {role: "owner"})
-        return res.status(200).json({success:true, message:"Now you can list cars"});
+        const { _id } = req.user;
+
+        await User.findByIdAndUpdate(_id, {
+            role: "owner",
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Now you can list cars",
+        });
+
     } catch (error) {
         console.log(error.message);
-        return res.status(500).json({success: false, message: error.message});
+
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
     }
-}
+};
+
+
+export const addCar = async (req, res) => {
+    try {
+        const { _id } = req.user;
+
+        const carData = JSON.parse(req.body.carData);
+
+        const imageFile = req.file;
+
+        if (!imageFile) {
+            return res.status(400).json({
+                success: false,
+                message: "Car image is required",
+            });
+        }
+
+        const response = await imageKit.files.upload({
+            file: imageFile.buffer.toString("base64"),
+            fileName: imageFile.originalname,
+            folder: "/cars",
+        });
+
+        const car = await Car.create({
+            owner: _id,
+            ...carData,
+            image: response.url,
+        });
+
+        return res.status(201).json({
+            success: true,
+            message: "Car added successfully",
+            car,
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
